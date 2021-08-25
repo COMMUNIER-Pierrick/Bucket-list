9a732885eec7acac30ef32d8e90adf4fdf79ab81
Pierrick Communier <pierrick.communier2021@campus-eni.fr>
2021-08-25 18:57:49 +0200

    installation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,8 +11,18 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      */
-    public function home(){
+    public function home():Response
+    {
 
         return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * @Route("/About us", name="main_about_us")
+     */
+    public function about_us():Response
+    {
+
+        return $this->render('main/about_us.html.twig');
     }
 }
